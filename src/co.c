1c0397f1cef070e70b577d831262aaf6b877e3e9
Tony Fettes <tonyfettes@tonyfettes.com>
2026-03-05 03:58:28 +0800

    refactor: rename mygo -> co

--- src/co.c
+++ src/co.c
@@ -1,7 +1,7 @@
 #include <moonbit.h>
 #include <stdint.h>
 
-typedef struct mygo_context {
+typedef struct moonbit_co_context {
   uint64_t rsp;
   uint64_t rbp;
   uint64_t rbx;
@@ -10,26 +10,28 @@ typedef struct mygo_context {
   uint64_t r14;
   uint64_t r15;
   uint64_t rax;
-} mygo_context_t;
+} moonbit_co_context_t;
 
 MOONBIT_FFI_EXPORT
-mygo_context_t *
-mygo_context_make(void) {
-  return (mygo_context_t *)moonbit_make_bytes(sizeof(mygo_context_t), 0);
+moonbit_co_context_t *
+moonbit_co_context_make(void) {
+  return (moonbit_co_context_t *)moonbit_make_bytes(
+    sizeof(moonbit_co_context_t), 0
+  );
 }
 
 MOONBIT_FFI_EXPORT
 void *
-mygo_stack_make(uint64_t size) {
+moonbit_co_stack_make(uint64_t size) {
   return (void *)moonbit_make_bytes((int32_t)size, 0);
 }
 
 extern void
-mygo_shift(mygo_context_t *from, mygo_context_t *to);
+moonbit_co_shift(moonbit_co_context_t *from, moonbit_co_context_t *to);
 
 extern void
-mygo__reset(
-  mygo_context_t *context,
+moonbit_co__reset(
+  moonbit_co_context_t *context,
   void *stack_top,
   void (*func)(void *),
   void *data
@@ -37,8 +39,8 @@ mygo__reset(
 
 MOONBIT_FFI_EXPORT
 void
-mygo_reset(
-  mygo_context_t *context,
+moonbit_co_reset(
+  moonbit_co_context_t *context,
   void *stack_base,
   uint64_t stack_size,
   void (*func)(void *),
@@ -46,5 +48,5 @@ mygo_reset(
 ) {
   uintptr_t sp = (uintptr_t)stack_base + stack_size;
   sp &= ~(uintptr_t)0xF;
-  mygo__reset(context, (void *)sp, func, data);
+  moonbit_co__reset(context, (void *)sp, func, data);
 }
